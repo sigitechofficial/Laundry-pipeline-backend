6f1e9006cdd38923eb7a77f94cffc53a9e63db89
alihamza-21 <alihamzaansari2122@gmail.com>
2025-04-22 12:32:45 +0500

    resolved pm2 issue

--- trigger_testing.php
+++ trigger_testing.php
@@ -11,7 +11,7 @@ $nodeBinPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin';
 
 // Set the PATH environment variable explicitly using putenv()
 putenv("PATH=$nodeBinPath:" . getenv('PATH'));
- 
+
 // Command to run npm install
 $npmCommand = "source /home/fomino/.nvm/nvm.sh && export HOME=/home/fomino && cd $workingDir && npm install";
 
@@ -36,27 +36,25 @@ if (is_resource($process)) {
     echo nl2br($installOutput);
 
     // Now, run the PM2 commands
-    // $process = proc_open($pm2Command, [
-    //     0 => ["pipe", "r"],
-    //     1 => ["pipe", "w"],
-    //     2 => ["pipe", "w"],
-    // ], $pipes);
+    $process = proc_open($pm2Command, [
+        0 => ["pipe", "r"],
+        1 => ["pipe", "w"],
+        2 => ["pipe", "w"],
+    ], $pipes);
 
-    // // Check if the PM2 process started successfully
-    // if (is_resource($process)) {
-    //     $pm2Output = stream_get_contents($pipes[1]);
-    //     fclose($pipes[1]);
-    //     fclose($pipes[2]);
-    //     proc_close($process);
+    // Check if the PM2 process started successfully
+    if (is_resource($process)) {
+        $pm2Output = stream_get_contents($pipes[1]);
+        fclose($pipes[1]);
+        fclose($pipes[2]);
+        proc_close($process);
 
-    //     echo "PM2 process started successfully.<br>";
-    //     echo nl2br($pm2Output);
-    // } else {
-    //     echo "Failed to start PM2 process.<br>";
-    // }
+        echo "PM2 process started successfully.<br>";
+        echo nl2br($pm2Output);
+    } else {
+        echo "Failed to start PM2 process.<br>";
+    }
 } else {
     echo "Failed to run npm install.<br>";
 }
 ?>
-
-
