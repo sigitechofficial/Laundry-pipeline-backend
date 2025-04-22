b3d7e143766d4a347a0bda864e1ef598720938d8
alihamza-21 <alihamzaansari2122@gmail.com>
2025-04-22 11:59:18 +0500

    resolved pm2 issue

--- trigger_testing.php
+++ trigger_testing.php
@@ -8,6 +8,7 @@ $workingDir = '/home/fomino/testlaundaryb.fomino.ch';
 
 // Set up the correct environment variables for the shell
 $nodeBinPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin';
+$pm2BinPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin';
 
 // Set the PATH environment variable explicitly using putenv()
 putenv("PATH=$nodeBinPath:" . getenv('PATH'));
