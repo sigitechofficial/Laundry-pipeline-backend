f4267dbe2761ba47e0b8c4f879712743f9b1c59a
alihamza-21 <alihamzaansari2122@gmail.com>
2025-04-22 12:15:08 +0500

    resolved pm2 issue

--- trigger_testing.php
+++ trigger_testing.php
@@ -8,7 +8,6 @@ $workingDir = '/home/fomino/testlaundaryb.fomino.ch';
 
 // Set up the correct environment variables for the shell
 $nodeBinPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin';
-$pm2BinPath = '/home/fomino/.nvm/versions/node/v16.20.2/bin';
 
 // Set the PATH environment variable explicitly using putenv()
 putenv("PATH=$nodeBinPath:" . getenv('PATH'));
@@ -59,3 +58,5 @@ if (is_resource($process)) {
     echo "Failed to run npm install.<br>";
 }
 ?>
+
+
