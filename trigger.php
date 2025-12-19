eb7373108f5e199a88fad2a623bf687a45d2d8ed
alihamza-21 <alihamzaansari2122@gmail.com>
2025-12-19 11:52:29 +0500

    change trigger.php script

--- trigger.php
+++ trigger.php
@@ -3,24 +3,25 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// ===== PATHS =====
+header('Content-Type: text/plain');
+
+// ===== CONFIG =====
 $workingDir = '/home/sigisolutions/laundarybackend.sigisolutions.net';
 $nodeBinPath = '/home/sigisolutions/.nvm/versions/node/v20.19.6/bin';
 
 // ===== ENV =====
-putenv("PATH=$nodeBinPath:" . getenv('PATH'));
 putenv("HOME=/home/sigisolutions");
+putenv("PATH=$nodeBinPath:" . getenv('PATH'));
 
-// ===== COMMAND (ZERO DOWNTIME) =====
+// ===== EXACT SAME COMMAND (AS TERMINAL) =====
 $command = "
-cd $workingDir || exit 1
-npm install
-pm2 reload laundary || pm2 start laundary.js --name laundary
+cd $workingDir && \
+npm install && \
+pm2 reload laundary || pm2 start laundary.js --name laundary && \
 pm2 save
 ";
 
 // ===== RUN =====
 $output = shell_exec($command . " 2>&1");
 
-header('Content-Type: text/plain');
 echo $output;
