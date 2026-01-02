e3711f424589fcf061141948bfacc67b9c3a1bab
pc10 <pc10@sigitechnologies.com>
2026-01-02 16:45:54 +0500

    Changes in transpoter

--- helper/transpoter.js
+++ helper/transpoter.js
@@ -2,10 +2,14 @@ const nodemailer=require('nodemailer')
 const transpoter=nodemailer.createTransport({
     host:process.env.EMAIL_HOST,
     port:process.env.EMAIL_PORT,
-    //secure:false,
+    secure: process.env.EMAIL_PORT == 465, // true for 465, false for other ports
     auth:{
         user:process.env.EMAIL_USERNAME,
         pass:process.env.EMAIL_PASSWORD
+    },
+    tls: {
+        // Do not fail on invalid certificates (for development/testing)
+        rejectUnauthorized: false
     }
 })
 
